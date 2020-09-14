87df3488243adb729d7caeef6c34d089e82a0c51
honaldu <hsh9636@gmail.com>
2020-09-14 15:09:10 +0900

    9월 14일

--- programmers/Python/caesarCipher.my.py
+++ programmers/Python/caesarCipher.my.py
@@ -1,15 +1,24 @@
 # 이터러블로 s 순환
 # 내부 if 문으로 알파벳, 공백 구분
+# elif 분으로 알파벳 대,소 문자 분기 생성
+# n 값에 따라 분기 생성
+
 # join 함수로 병합
 def solution(s, n):
-    wordsList = []
+    wordsString = ""
     for i in s:
+        # 공백 시
         if i == " ":
-            wordsList.append(i)
-        elif ord(i) + n > 122:
-            97(ord(i)+n)-122
-        wordsList.append()
-    return str(''.join([chr(ord(i)+n) for i in s if ord(i)+n > 96]))
+            wordsString += i
+        # 소문자
+        elif(ord(i) > 96):
+            A = chr(ord(i)+n)
+            wordsString += A
+        # 소문자 시
+        else:
+            A = chr(ord(i)+n)
+            wordsString += A
+    print(wordsString)
 
 
 solution("AB", 1)
