343ee001e7e61338c749eb1607f93f7037316a45
honaldu <hsh9636@gmail.com>
2020-10-01 23:09:12 +0900

    10월 1일

--- programmers/Python/2016.my.py
+++ programmers/Python/2016.my.py
@@ -1,18 +1,12 @@
-def solution(a,b):
-    basic = 2
-    month = None
-    date = None
-    if a==(1 or 10):
-        month = 1
-    elif a == (2 or 3):
-        month = 4
-    elif a == 4 :
-        month = 7
-    elif a ==5 :
-        month = 2
-    elif a == 6:
-        month = 5
-    
-    return month
+# 2016년 각 달의 날일 수 list
+# 요일 list
+# a의 전달 sum 함수로 합계 에 b를 더함
+# 더한 수의 나머지로 weakends 의 index 처리
+def solution(a, b):
 
-print(solution(10,10))
+    months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    weakends = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
+    return weakends[(sum(months[0:a-1])+b) % 7]
+
+
+print(solution(5, 24))
