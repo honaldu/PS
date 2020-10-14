22ff55399053072fbf6ed56bff72517cb5635378
허석호 <microcephalus7@gmail.com>
2020-10-14 10:51:17 +0900

    10월 13일 분

--- leetCode/python/greedy/1046.my.py
+++ leetCode/python/greedy/1046.my.py
@@ -1,42 +1,27 @@
-# while 문 돌림
+# while 문
+# list 길이에 따라 breake
 # list 안에서 최대값 2개 추출 (추출된 값 버림)
 # 최대값 각각 뺀 수 list 안에 삽입 (뺀 수 0일 시 삽입 안 함)
-#
+
 
 from typing import List
 
 
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        first = max(stones)
-        stones.remove(max(stones))
-        second = max(stones)
-        stones.remove(max(stones))
-        smashed = first-second
-        if smashed != 0:
-            stones.append(smashed)
-        first = max(stones)
-        stones.remove(max(stones))
-        second = max(stones)
-        stones.remove(max(stones))
-        smashed = first-second
-        if smashed != 0:
-            stones.append(smashed)
-        first = max(stones)
-        stones.remove(max(stones))
-        second = max(stones)
-        stones.remove(max(stones))
-        smashed = first-second
-        if smashed != 0:
-            stones.append(smashed)
-        first = max(stones)
-        stones.remove(max(stones))
-        second = max(stones)
-        stones.remove(max(stones))
-        smashed = first-second
-        if smashed != 0:
-            stones.append(smashed)
-        return stones
+        while True:
+            if(len(stones) == 1 or len(stones) == 0):
+                break
+            y = max(stones)
+            stones.remove(y)
+            x = max(stones)
+            stones.remove(x)
+            if y != x:
+                stones.append(y-x)
+        if stones == []:
+            return 0
+        else:
+            return stones[0]
 
 
 solution = Solution()
